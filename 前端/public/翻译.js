aba490bcd09fbbc43099999de510ada5d067a062
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-09-22 11:19:49 -0500

    使用演示ip地址, 暂时避免CORS问题

--- 前端/public/翻译.js
+++ 前端/public/翻译.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("#翻译按钮").click(function() {
 		$("#对应中文源码").val("处理中...")
-		$.post("http://localhost:8091/", {
+		$.post("http://74.91.17.250:8091/", {
 			code : $("#英文源码输入").val()
 		}, function(结果) {
 			$("#对应中文源码").val(结果.内容);
